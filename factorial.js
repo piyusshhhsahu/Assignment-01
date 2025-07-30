b520be426e3b3a9094ab1ee7701987ecef6f7f0c
piyush sahu <Piyushsahu2292@gmail.com>
2025-07-30 20:48:51 +0530

    added second question so;ution

---
+++ factorial.js
@@ -0,0 +1,8 @@
+function factorial(n) {
+    if (n < 0) return undefined; // Factorial is not defined for negative numbers
+    if (n === 0 || n === 1) return 1;
+    return n * factorial(n - 1);
+  }
+  
+  module.exports = factorial;
+  
